4ebaa6059a7e8ae1342e2a219f906fc7d6960b2e
manekajal <manekajal2014@gmail.com>
2021-10-12 09:02:52 +0530

    [Kajal]: Lambda Expression

---
+++ src/main/java/com/bl/RegexLambdaExpression.java
@@ -0,0 +1,18 @@
+package com.bl;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface Regex{
+
+    boolean details(String input);
+}
+
+public class RegexLambdaExpression {
+
+    public static void main(String[] args){
+        System.out.println(" Lambda Expression ");
+
+    }
+}
