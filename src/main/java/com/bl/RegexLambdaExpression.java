883d7fdd257514af008f6cb4cd4efe1912e2fd13
manekajal <manekajal2014@gmail.com>
2021-10-12 09:04:25 +0530

    [Kajal]: Lambda Expression

--- src/main/java/com/bl/RegexLambdaExpression.java
+++ src/main/java/com/bl/RegexLambdaExpression.java
@@ -13,6 +13,26 @@ public class RegexLambdaExpression {
 
     public static void main(String[] args){
         System.out.println(" Lambda Expression ");
+        Regex regex =(input)->{
+            String pattern= "^[A-z]{3,}";
+            Pattern r= Pattern.compile(pattern);
+
+            Matcher m = r.matcher(input);
+
+
+            if(m.matches()){
+                System.out.println("First Name Matched :" );
+
+            }else
+            {
+                System.out.println(" First Name No Matched :");
+            }
+            return true;
+        };
+
+        System.out.println("First Name :" +regex.details("Kajal"));
+
+
 
     }
 }
